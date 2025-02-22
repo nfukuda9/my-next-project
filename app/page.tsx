b4439901cb6af7d79132f97fef6db431339ed29d
nfukuda9 <nfukuda9@gmail.com>
2025-02-22 16:28:10 +0900

    02221628コミット

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1 className={style.title}>テクノロジーの力で世界を変える</h1>
+      <h1 className={style.title}>テクノロジーの力で世界を変える！！！”</h1>
       <p className={style.description}>私達は市場をリードしているグローバルテックカンパニーです。</p>
     </div>
   );
